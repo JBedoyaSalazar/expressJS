051a7d7a7978e6f708e96633279c897161f9da91
Jefred Bedoya <jefred03salazar@gmail.com>
2026-06-10 18:01:10 -0500

    feat: implement user management endpoints with validation; add users.json for data storage

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 
+const fs = require('fs')
+const path = require('path')
+const { error } = require('console')
+const usersFilePath = path.join(__dirname, 'users.json')
+
 const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -45,8 +50,8 @@ app.post('/form', (req, res) => {
 
 app.post('/api/data', (req, res) => {
     const data = req.body
-    if(!data || Object.keys(data).length === 0){
-        return res.status(400).json( {error: 'No se han recibido datos'})
+    if (!data || Object.keys(data).length === 0) {
+        return res.status(400).json({ error: 'No se han recibido datos' })
     }
 
     res.status(201).json({
@@ -55,6 +60,123 @@ app.post('/api/data', (req, res) => {
     })
 })
 
+app.get('/users', (req, res) => {
+    fs.readFile(usersFilePath, 'utf-8', (err, data) => {
+        if (err) {
+            return res.status(500).json({
+                error: `Error con conexion de datos:
+                ${err}
+            ` })
+        }
+        const users = JSON.parse(data)
+        res.json(users)
+    })
+})
+
+app.post('/users', (req, res) => {
+    const { name, email } = req.body
+    const validation = validateUser(name, email)
+
+    if (!validation.valid) {
+        return res.status(400).json({
+            error: validation.message
+        })
+    }
+
+    const newUser = req.body
+    fs.readFile(usersFilePath, 'utf-8', (err, data) => {
+        if (err) {
+            return res.status(500).json({
+                error: `Error con conexion de datos:
+                ${err}
+                `
+            })
+        }
+
+        const users = JSON.parse(data)
+        users.push(newUser)
+        fs.writeFile(usersFilePath, JSON.stringify(users, null, 2), (err) => {
+            if (err) {
+                return res.status(500).json({
+                    error: `Error con conexion de datos:
+                    ${err}
+                    `
+                })
+            }
+            res.status(201).json(newUser)
+        })
+    })
+})
+
+app.put('/users/:id', (req, res) => {
+    const userId = parseInt(req.params.id, 10)
+    const updatedUser = req.body
+
+    const { name, email } = req.body
+    const validation = validateUser(name, email)
+
+    if (!validation.valid) {
+        return res.status(400).json({
+            error: validation.message
+        })
+    }
+
+    fs.readFile(usersFilePath, 'utf-8', (err, data) => {
+        if (err) {
+            return res.status(500).json({
+                Error: `Error desde el server ${err}`
+            })
+        }
+
+        let users = JSON.parse(data)
+
+        const userExists = users.some(
+            user => user.id === userId
+        )
+
+        if (!userExists) {
+            return res.status(404).json({
+                error: 'User not found'
+            })
+        }
+        users = users.map(user => (user.id === userId ? { ...user, ...updatedUser } : user))
+        fs.writeFile(
+            usersFilePath,
+            JSON.stringify(users, null, 2),
+            (err) => {
+                if (err) {
+                    return res.status(500).json({
+                        Error: `Error al actualizar el usuario: ${err}`
+                    })
+                }
+                res.json(updatedUser)
+            })
+    })
+
+})
+
 app.listen(PORT, () => {
     console.log(`Listening On http://localhost:${PORT}`)
 })
+
+function validateUser(name, email) {
+    if (!name || name.length < 3 || name.length > 20) {
+        return {
+            valid: false,
+            message: 'El nombre debe tener entre 3 y 20 caracteres'
+        }
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
+    if (!emailRegex.test(email)) {
+        return {
+            valid: false,
+            message: 'Invalid email format'
+        }
+    }
+
+    return {
+        valid: true
+    }
+}
