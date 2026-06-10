cd0b5904c16d955afae7ad2605b0edfd86e2c3c9
Jefred Bedoya <jefred03salazar@gmail.com>
2026-06-09 23:45:22 -0500

    feat: add body-parser middleware and update package.json for dependency management

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 require('dotenv').config()
 const express = require('express')
-const app = express()
+const bodyParser = require('body-parser')
 
-const PORT = process.env.PORT || 3000
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+const PORT = process.env.PORT
 
 app.get('/', (req, res) => {
     res.send(`
@@ -10,6 +14,45 @@ app.get('/', (req, res) => {
         <p>Aplicativo NodeJS con express</p>
         <p>Running on ${PORT}</p>
         `)
+})
+
+app.get('/users/:id', (req, res) => {
+    const userId = req.params.id
+    res.send(`Mostrar informacion del usuarion con ID: ${userId}`)
+})
+
+app.get('/search', (req, res) => {
+    const terms = req.query.terms || `No especificado`
+    const category = req.query.category || `Todas`
+    res.send(`
+        <h2>Resultados De Busqueda</h2>
+        <p>Termino: ${terms}</p>
+        <p>Categoria: ${category}</p>
+        `)
+})
+
+app.post('/form', (req, res) => {
+    const name = req.body.nombre || `Anonimo`
+    const email = req.body.email || `N/G`
+    res.json({
+        message: `Datos recibidos con exito`,
+        data: {
+            name,
+            email
+        }
+    })
+})
+
+app.post('/api/data', (req, res) => {
+    const data = req.body
+    if(!data || Object.keys(data).length === 0){
+        return res.status(400).json( {error: 'No se han recibido datos'})
+    }
+
+    res.status(201).json({
+        message: "Datos JSON recibidos",
+        data
+    })
 })
 
 app.listen(PORT, () => {
